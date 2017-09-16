51da5b40eb042ff5f1dd8999824860833aa2d36f
Oliver Spring <oliver9589@gmail.com>
2017-09-16 22:22:34 +0000

    automatically backed up by learn

--- src/index.js
+++ src/index.js
@@ -32,7 +32,8 @@ export class InFrontOfYou extends React.Component {
 export class ButcherShop extends React.Component {
   render() {
     return React.createElement('ul',{},
-      React.createElement('li', {}, BUTCHER_PRODUCTS.map())
+      React.createElement('li', {}, BUTCHER_PRODUCTS.map((meat) => 
+        <li meat />))
     );
   }
 };
